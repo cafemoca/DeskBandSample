6444997612607ee303bd9fbba522e52d51870bdb
cafemoca <dev@moca.cm>
2017-06-20 20:05:48 +0900

    Tweak names

--- DeskBandSample/DeskBandSample.cs
+++ DeskBandSample/DeskBandSample.cs
@@ -1,39 +1,40 @@
 ﻿using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using Cafemoca.DeskBandSample.DeskBand;
 
 namespace Cafemoca.DeskBandSample
 {
     [Guid("6E26E58A-D2E1-4001-AB88-02CFE62CD668"), ComVisible(true)]
-    [BandObject("SampleBand", BandObjectStyle.TaskbarToolBar)]
-    public class SampleBand : BandObject
+    [BandObject("DeskBandSample", BandObjectStyle.TaskbarToolBar)]
+    public class DeskBandSample : BandObject
     {
-        private System.Windows.Forms.Button button;
+        private Button _button;
 
-        public SampleBand()
+        public DeskBandSample()
         {
             this.InitializeComponent();
         }
 
         private void InitializeComponent()
         {
-            this.button = new System.Windows.Forms.Button();
+            this._button = new Button();
             this.SuspendLayout();
             // 
             // button
             // 
-            this.button.Location = new Point(0, 0);
-            this.button.Name = "button";
-            this.button.Size = new Size(150, 20);
-            this.button.TabIndex = 0;
-            this.button.Text = "DeskBandSample";
-            this.button.UseVisualStyleBackColor = true;
+            this._button.Location = new Point(0, 0);
+            this._button.Name = "_button";
+            this._button.Size = new Size(150, 20);
+            this._button.TabIndex = 0;
+            this._button.Text = "DeskBandSample";
+            this._button.UseVisualStyleBackColor = true;
             // 
-            // SampleBand
+            // DeskBandSample
             // 
-            this.Controls.Add(this.button);
+            this.Controls.Add(this._button);
             this.MinSize = new Size(150, 20);
-            this.Name = "SampleBand";
+            this.Name = "DeskBandSample";
             this.Size = new Size(150, 20);
             this.Title = "DeskBandSample";
             this.ResumeLayout(false);
