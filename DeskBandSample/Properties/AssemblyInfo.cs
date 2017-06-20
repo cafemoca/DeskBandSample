b63f5d5f1f3d53d9d9f92346ad68fcc1252da0aa
cafemoca <dev@moca.cm>
2017-06-20 19:20:16 +0900

    Enable to "Make assembly COM-Visible"

--- DeskBandSample/Properties/AssemblyInfo.cs
+++ DeskBandSample/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fa609c38-57c1-4e79-be69-9a3cd5defb87")]
